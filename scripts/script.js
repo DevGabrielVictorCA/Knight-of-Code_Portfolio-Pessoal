cb82dc8f93a18e796dc3af0f7e6e3cb66049492b
Gabriel Cardoso <gabrielvictorcardosopeace@gmail.com>
2025-08-13 23:53:23 -0300

    adicionar melhorias

--- scripts/script.js
+++ scripts/script.js
@@ -13,7 +13,7 @@ abrirMenu.addEventListener('click', () => {
 
 document.addEventListener('click', (event) => {
     if (!event.target.closest('.nav-items') && !event.target.closest('.abrir-menu')){
-            abrirMenu.classList.toggle('open');
+            abrirMenu.classList.remove('open');
             navItems.classList.remove('active');
             voltarTopo.classList.remove('topoInativo');
     }
